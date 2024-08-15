5fcb5bc0fcc14194542a5f831695e619a74eb319
Diogo-Garcia11 <diogo.garcia3@etec.sp.gov.br>
2024-08-14 22:31:58 -0300

    mudei as configuracao pra ficarem mais simples e testei o framework

---
+++ TCC-Concursador-Site/resources/views/teste.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>teste</title>
+</head>
+<body>
+    <nav style="background-color:aqua; height:50px">testando</nav>
+    <main style="background-color:aliceblue; height:50px">a</main>
+    <footer style="background-color:brown; height:50px">bomba</footer>
+</body>
+</html>
