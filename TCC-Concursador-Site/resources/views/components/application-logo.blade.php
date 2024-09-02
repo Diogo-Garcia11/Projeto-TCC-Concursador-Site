77114f62849cb6bdda60fe9bc07239078d204778
Diogo Garcia <diogo.garcia3@etec.sp.gov.br>
2024-09-02 17:00:05 -0300

    criando a logica das questoes

--- TCC-Concursador-Site/resources/views/components/application-logo.blade.php
+++ TCC-Concursador-Site/resources/views/components/application-logo.blade.php
@@ -1,3 +1,3 @@
-<a href="/"><img src="{{ asset('imagens/logo.ico') }}" class="w-auto h-auto" ></a>
+<a href="/dashboard"><img src="{{ asset('imagens/logo.ico') }}" class="w-auto h-auto" ></a>
 
 
--- TCC-Concursador-Site/routes/web.php
+++ TCC-Concursador-Site/routes/web.php
@@ -15,7 +15,7 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::get('/simulados', [SimuladoController::class, 'index'])->name('simulado');
+Route::get('/simulados', [SimuladoController::class], 'index');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
