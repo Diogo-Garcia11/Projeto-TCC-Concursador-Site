dae54f375bde51d980105f738805bac32aba1194
Ket <ketelilino@gmail.com>
2024-09-05 15:26:48 -0300

    sfsfsf

--- TCC-Concursador-Site/resources/views/materias.blade.php
+++ TCC-Concursador-Site/resources/views/materias.blade.php
@@ -3,78 +3,6 @@
 <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
 
 <x-app-layout>
-<<<<<<< HEAD
-    <div class="flex flex-col items-center justify-center min-h-screen pt-32 pb-20"> <!-- Centralizar verticalmente -->
-        <div class="max-w-6xl mx-auto sm:px-6 lg:px-8">
-            <!-- Container grid para os itens -->
-            <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4"> <!-- Aumentado o espaçamento entre os cards -->
-                <!-- Item 1 -->
-                <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg p-12 w-full h-80">
-                    <div class="flex flex-col items-center">
-                        <img src="{{asset('imagens/mat.jpg')}}" alt="Placeholder Image" class="mb-8 w-64 h-64 object-cover">
-                        <a href="{{route('simulados.fuvest')}}" class="text-xl font-semibold hover:underline">Matemática</a>
-                    </div>
-                </div>
-                <!-- Item 2 -->
-                <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg p-12 w-full h-80">
-                    <div class="flex flex-col items-center">
-                        <img src="{{asset('imagens/port.jpg')}}" alt="Placeholder Image" class="mb-8 w-64 h-64 object-cover">
-                        <a href="{{route('simulados.fatec')}}" class="text-xl font-semibold hover:underline">Português</a>
-                    </div>
-                </div>
-                <!-- Item 3 -->
-                <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg p-12 w-full h-80">
-                    <div class="flex flex-col items-center">
-                        <img src="{{asset('imagens/lit.jpg')}}" alt="Placeholder Image" class="mb-8 w-64 h-64 object-cover">
-                        <a href="{{route('simulados.fatec')}}" class="text-xl font-semibold hover:underline">Literatura</a>
-                    </div>
-                </div>
-                <!-- Item 4 -->
-                <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg p-12 w-full h-80">
-                    <div class="flex flex-col items-center">
-                        <img src="{{asset('imagens/fil.jpg')}}" alt="Placeholder Image" class="mb-8 w-64 h-64 object-cover">
-                        <a href="{{route('simulados.fatec')}}" class="text-xl font-semibold hover:underline">Filosofia</a>
-                    </div>
-                </div>
-                <!-- Item 5 -->
-                <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg p-12 w-full h-80">
-                    <div class="flex flex-col items-center">
-                        <img src="https://via.placeholder.com/300" alt="Placeholder Image" class="mb-8 w-64 h-64 object-cover">
-                        <a href="{{route('simulados.fatec')}}" class="text-xl font-semibold hover:underline">Biologia</a>
-                    </div>
-                </div>
-                <!-- Item 6 -->
-                <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg p-12 w-full h-80">
-                    <div class="flex flex-col items-center">
-                        <img src="https://via.placeholder.com/300" alt="Placeholder Image" class="mb-8 w-64 h-64 object-cover">
-                        <a href="{{route('simulados.fatec')}}" class="text-xl font-semibold hover:underline">Física</a>
-                    </div>
-                </div>
-                <!-- Item 7 -->
-                <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg p-12 w-full h-80">
-                    <div class="flex flex-col items-center">
-                        <img src="https://via.placeholder.com/300" alt="Placeholder Image" class="mb-8 w-64 h-64 object-cover">
-                        <a href="{{route('simulados.fatec')}}" class="text-xl font-semibold hover:underline">Química</a>
-                    </div>
-                </div>
-                <!-- Item 8 -->
-                <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg p-12 w-full h-80">
-                    <div class="flex flex-col items-center">
-                        <img src="{{asset('imagens/his.jpg')}}" class="mb-8 w-64 h-64 object-cover">
-                        <a href="{{route('simulados.fatec')}}" class="text-xl font-semibold hover:underline">História</a>
-                    </div>
-                </div>
-                <!-- Item 9 -->
-                <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg p-12 w-full h-80">
-                    <div class="flex flex-col items-center">
-                        <img src="{{asset('imagens/geo.jpg')}}" alt="Placeholder Image" class="mb-8 w-64 h-64 object-cover">
-                        <a href="{{route('simulados.fatec')}}" class="text-xl font-semibold hover:underline">Geografia</a>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-=======
   <div class="text-center">
     <h5 class="text-7xl font-Montserrat text-white p-8">MATÉRIAS</h5>
   </div>
@@ -165,6 +93,5 @@
     </script>
 
   </main>
->>>>>>> 9e63cd4176ef15e330d32843b6cd6da4717172ef
 </x-app-layout>
 
