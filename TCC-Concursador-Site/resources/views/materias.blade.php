24bb82747edab7515d0fe4f4d4f2c254f5743928
Ket <ketelilino@gmail.com>
2024-09-12 15:22:35 -0300

    Update materias.blade.php

--- TCC-Concursador-Site/resources/views/materias.blade.php
+++ TCC-Concursador-Site/resources/views/materias.blade.php
@@ -1,7 +1,6 @@
 <link rel="preconnect" href="https://fonts.googleapis.com">
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-<link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap"
-  rel="stylesheet">
+<link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
 
 <x-app-layout>
   <div class="text-center">
@@ -13,10 +12,8 @@
       <input type="checkbox" class="peer absolute top-0 inset-x-0 w-full h-40 opacity-0 z-10 cursor-pointer">
       <div class="bg-steelblue h-40 w-full pl-5 flex items-center">
         <div class=" left-3 ">
-          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" class="w-20 h-20"
-            stroke="currentColor" class="size-6">
-            <path stroke-linecap="round" stroke-linejoin="round"
-              d="M9.75 3.104v5.714a2.25 2.25 0 0 1-.659 1.591L5 14.5M9.75 3.104c-.251.023-.501.05-.75.082m.75-.082a24.301 24.301 0 0 1 4.5 0m0 0v5.714c0 .597.237 1.17.659 1.591L19.8 15.3M14.25 3.104c.251.023.501.05.75.082M19.8 15.3l-1.57.393A9.065 9.065 0 0 1 12 15a9.065 9.065 0 0 0-6.23-.693L5 14.5m14.8.8 1.402 1.402c1.232 1.232.65 3.318-1.067 3.611A48.309 48.309 0 0 1 12 21c-2.773 0-5.491-.235-8.135-.687-1.718-.293-2.3-2.379-1.067-3.61L5 14.5" />
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" class="w-20 h-20" stroke="currentColor" class="size-6">
+            <path stroke-linecap="round" stroke-linejoin="round" d="M9.75 3.104v5.714a2.25 2.25 0 0 1-.659 1.591L5 14.5M9.75 3.104c-.251.023-.501.05-.75.082m.75-.082a24.301 24.301 0 0 1 4.5 0m0 0v5.714c0 .597.237 1.17.659 1.591L19.8 15.3M14.25 3.104c.251.023.501.05.75.082M19.8 15.3l-1.57.393A9.065 9.065 0 0 1 12 15a9.065 9.065 0 0 0-6.23-.693L5 14.5m14.8.8 1.402 1.402c1.232 1.232.65 3.318-1.067 3.611A48.309 48.309 0 0 1 12 21c-2.773 0-5.491-.235-8.135-.687-1.718-.293-2.3-2.379-1.067-3.61L5 14.5" />
           </svg>
 
         </div>
@@ -27,42 +24,42 @@
       <div class="bg-whiteblue overflow-hidden transition-all duration-500 max-h-0 peer-checked:max-h-[500px]">
         <div class="p-4 grid grid-cols-3 grid-rows-3 gap-4 place-items-center">
 
-        <div class="grid-item">
-          <a href="" class="text-3xl font-Montserrat text-steelblue p-8">➤ Atomística</a>
-        </div>
-        <div class="grid-item">
-          <a href="" class="text-3xl font-Montserrat text-steelblue p-8">➤ Tabela periódica</a>
-        </div>
-        <div class="grid-item">
-          <a href="" class="text-3xl font-Montserrat text-steelblue p-8">➤ Ligações químicas</a>
-        </div>
-        <div class="grid-item">
-          <a href="" class="text-3xl font-Montserrat text-steelblue p-8">➤ Oxirreduções</a>
-        </div>
-        <div class="grid-item">
-          <a href="" class="text-3xl font-Montserrat text-steelblue p-8">➤ Ligações químicas</a>
-        </div>
-        <div class="grid-item">
-          <a href="" class="text-3xl font-Montserrat text-steelblue p-8">➤ Eletroquímica</a>
-        </div>
-        <div class="grid-item">
-          <a href="" class="text-3xl font-Montserrat text-steelblue p-8">➤ Radioatividade</a>
-        </div>
-        <div class="grid-item">
-          <a href="" class="text-3xl font-Montserrat text-steelblue p-8">➤ Soluções químicas</a>
-        </div>
-        <div class="grid-item">
-          <a href="" class="text-3xl font-Montserrat text-steelblue p-8">➤ gfgfdgdf</a>
-        </div>
-        <div class="grid-item">
-          <a href="" class="text-3xl font-Montserrat text-steelblue p-8">➤ gfdgd</a>
-        </div>
-        <div class="grid-item">
-          <a href="" class="text-3xl font-Montserrat text-steelblue p-8">➤ gddfgdf</a>
-        </div>
-        <div class="grid-item">
-          <a href="" class="text-3xl font-Montserrat text-steelblue p-8">➤ gdfgd</a>
-        </div>
+          <div class="grid-item">
+            <a href="" class="text-3xl font-Montserrat text-steelblue p-8">➤ Atomística</a>
+          </div>
+          <div class="grid-item">
+            <a href="" class="text-3xl font-Montserrat text-steelblue p-8">➤ Tabela periódica</a>
+          </div>
+          <div class="grid-item">
+            <a href="" class="text-3xl font-Montserrat text-steelblue p-8">➤ Ligações químicas</a>
+          </div>
+          <div class="grid-item">
+            <a href="" class="text-3xl font-Montserrat text-steelblue p-8">➤ Oxirreduções</a>
+          </div>
+          <div class="grid-item">
+            <a href="" class="text-3xl font-Montserrat text-steelblue p-8">➤ Ligações químicas</a>
+          </div>
+          <div class="grid-item">
+            <a href="" class="text-3xl font-Montserrat text-steelblue p-8">➤ Eletroquímica</a>
+          </div>
+          <div class="grid-item">
+            <a href="" class="text-3xl font-Montserrat text-steelblue p-8">➤ Radioatividade</a>
+          </div>
+          <div class="grid-item">
+            <a href="" class="text-3xl font-Montserrat text-steelblue p-8">➤ Soluções químicas</a>
+          </div>
+          <div class="grid-item">
+            <a href="" class="text-3xl font-Montserrat text-steelblue p-8">➤ gfgfdgdf</a>
+          </div>
+          <div class="grid-item">
+            <a href="" class="text-3xl font-Montserrat text-steelblue p-8">➤ gfdgd</a>
+          </div>
+          <div class="grid-item">
+            <a href="" class="text-3xl font-Montserrat text-steelblue p-8">➤ gddfgdf</a>
+          </div>
+          <div class="grid-item">
+            <a href="" class="text-3xl font-Montserrat text-steelblue p-8">➤ gdfgd</a>
+          </div>
 
 
         </div>
@@ -73,23 +70,68 @@
     <div class="relative w-[80%] overflow-hidden">
       <input type="checkbox" class="peer absolute top-0 inset-x-0 w-full h-40 opacity-0 z-10 cursor-pointer">
       <div class="bg-kblue h-40 w-full pl-5 flex items-center">
-        <div class=" left-3 ">
-          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" class="w-20 h-20"
-            stroke="currentColor" class="size-6">
-            <path stroke-linecap="round" stroke-linejoin="round"
-              d="M9.75 3.104v5.714a2.25 2.25 0 0 1-.659 1.591L5 14.5M9.75 3.104c-.251.023-.501.05-.75.082m.75-.082a24.301 24.301 0 0 1 4.5 0m0 0v5.714c0 .597.237 1.17.659 1.591L19.8 15.3M14.25 3.104c.251.023.501.05.75.082M19.8 15.3l-1.57.393A9.065 9.065 0 0 1 12 15a9.065 9.065 0 0 0-6.23-.693L5 14.5m14.8.8 1.402 1.402c1.232 1.232.65 3.318-1.067 3.611A48.309 48.309 0 0 1 12 21c-2.773 0-5.491-.235-8.135-.687-1.718-.293-2.3-2.379-1.067-3.61L5 14.5" />
+        <div class=" right-3 ">
+          <svg fill="#000000" height="200px" width="200px" version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 512 512" xml:space="preserve" class="w-20 h-20">
+            <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+            <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g>
+            <g id="SVGRepo_iconCarrier">
+              <g>
+                <g>
+                  <g>
+                    <path d="M256.275,205.17c-27.949,0-50.603,22.662-50.603,50.594c0,27.936,22.668,50.603,50.603,50.603 c27.941,0,50.603-22.653,50.603-50.603C306.878,227.837,284.22,205.17,256.275,205.17z"></path>
+                    <path d="M256.3,44.455c-23.637-13.458-47.304-24.196-70.389-31.787c-12.686-4.176-26.381,3.097-29.91,16.16 c-3.267,12.092,3.984,24.548,15.873,28.49c12.775,4.235,25.923,9.697,39.242,16.23c-51.77,37.201-99.895,85.332-137.087,137.098 C47.774,156.549,33.982,94.305,64.396,63.891c9.138-9.138,9.138-23.954,0-33.092c-9.137-9.138-23.954-9.138-33.092,0 C-24.875,86.976,3.838,183.16,44.987,255.753c-89.167,156.468-33.874,255.815,69.306,255.815 c104.938,0,241.248-95.3,324.215-210.633c3.97,8.16,7.614,16.397,10.858,24.687c41.986,107.294-0.223,165.513-110.716,127.914 c-8.444-2.874-11.708-5.262-16.035-2.047c-9.95,7.394-19.993,14.366-30.088,20.893c-5.189,3.354-4.543,11.149,1.101,13.666 c191.832,85.529,276.638-53.087,174.417-231.041C582.887,52.447,458.828-70.927,256.3,44.455z M256.011,412.138 c-83.936-54.836-134.94-126.304-134.94-126.304c-3.363-4.393-10.12-4.037-12.909,0.743 c-6.075,10.404-11.503,20.782-16.238,31.061c-1.229,2.668-0.825,5.813,1.017,8.101c0,0,51.659,64.982,117.243,112.016 C-47.801,561.764,24.253,251.004,256.165,99.344c61.828,40.399,118.463,97.768,157.218,157.399 C372.233,321.098,314.002,375.247,256.011,412.138z M439.026,210.447c-34.891-47.873-83.797-98.263-137.625-136.962 C433.954,8.778,508.583,62.505,439.026,210.447z"></path>
+                    <path d="M104.811,47.14c2.092,0,6.1-0.534,14.123-0.248c12.881,0.498,23.76-9.631,24.222-22.547 c0.463-12.915-9.631-23.76-22.547-24.222c-5.936-0.214-11.798-0.12-17.416,0.275c-12.892,0.906-22.608,12.092-21.7,24.985 C82.361,37.715,92.635,47.14,104.811,47.14z"></path>
+                  </g>
+                </g>
+              </g>
+            </g>
           </svg>
-
         </div>
         <h1 class="text-5xl font-Montserrat text-steelblue p-10 text-right">FÍSICA</h1>
       </div>
 
       <!-- Conteúdo -->
-      <div class="bg-whiteblue overflow-hidden transition-all duration-500 max-h-0 peer-checked:max-h-40">
-        <div class="p-4">
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime voluptates similique voluptatem autem
-            dolorem quam, vero, obcaecati non placeat necessitatibus, cumque exercitationem quod ipsam iusto molestias
-            eligendi sed deleniti soluta.</p>
+      <div class="bg-whiteblue overflow-hidden transition-all duration-500 max-h-0 peer-checked:max-h-[500px]">
+        <div class="p-4 grid grid-cols-3 grid-rows-3 gap-4 place-items-center">
+
+          <div class="grid-item">
+            <a href="" class="text-3xl font-Montserrat text-steelblue p-8">➤ Mecânica</a>
+          </div>
+          <div class="grid-item">
+            <a href="" class="text-3xl font-Montserrat text-steelblue p-8">➤ Tabela periódica</a>
+          </div>
+          <div class="grid-item">
+            <a href="" class="text-3xl font-Montserrat text-steelblue p-8">➤ Ligações químicas</a>
+          </div>
+          <div class="grid-item">
+            <a href="" class="text-3xl font-Montserrat text-steelblue p-8">➤ Oxirreduções</a>
+          </div>
+          <div class="grid-item">
+            <a href="" class="text-3xl font-Montserrat text-steelblue p-8">➤ Ligações químicas</a>
+          </div>
+          <div class="grid-item">
+            <a href="" class="text-3xl font-Montserrat text-steelblue p-8">➤ Eletroquímica</a>
+          </div>
+          <div class="grid-item">
+            <a href="" class="text-3xl font-Montserrat text-steelblue p-8">➤ Radioatividade</a>
+          </div>
+          <div class="grid-item">
+            <a href="" class="text-3xl font-Montserrat text-steelblue p-8">➤ Soluções químicas</a>
+          </div>
+          <div class="grid-item">
+            <a href="" class="text-3xl font-Montserrat text-steelblue p-8">➤ gfgfdgdf</a>
+          </div>
+          <div class="grid-item">
+            <a href="" class="text-3xl font-Montserrat text-steelblue p-8">➤ gfdgd</a>
+          </div>
+          <div class="grid-item">
+            <a href="" class="text-3xl font-Montserrat text-steelblue p-8">➤ gddfgdf</a>
+          </div>
+          <div class="grid-item">
+            <a href="" class="text-3xl font-Montserrat text-steelblue p-8">➤ gdfgd</a>
+          </div>
+
+
         </div>
       </div>
     </div>
