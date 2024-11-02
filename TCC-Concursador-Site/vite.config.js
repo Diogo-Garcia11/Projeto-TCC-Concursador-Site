509f5c2daf2567fd5894e613ec4605d8755225f5
Kevyn Banks <kevynbanks13@gmail.com>
2024-11-02 14:19:16 -0300

    problema do css
    tive que realocar o css do simulado.css pro app.css novamente pro site rodar, apenas testes e testes

--- TCC-Concursador-Site/vite.config.js
+++ TCC-Concursador-Site/vite.config.js
@@ -8,7 +8,6 @@ export default defineConfig({
                 'resources/css/app.css',
                 'resources/js/app.js',
                 'resources/js/simulados.js',
-                'resources/js/simulados.css'
             ],
             refresh: true,
         }),
