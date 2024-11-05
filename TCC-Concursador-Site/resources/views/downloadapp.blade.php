e8031f5882c82f93d0f4ce38016a175736915771
Ket <ketelilino@gmail.com>
2024-11-04 23:08:21 -0300

    .

--- TCC-Concursador-Site/resources/views/downloadapp.blade.php
+++ TCC-Concursador-Site/resources/views/downloadapp.blade.php
@@ -23,16 +23,17 @@
         }
     </style>
     
-    <div class="relative inline-block h-screen">
-        <img class="w-full h-auto" src="{{ asset('imagens/meiocirculo.png') }}" alt="Semicirculo">
+    <div class="relative inline-block h-screen flex">
+        <img class="" src="{{ asset('imagens/meiocirculo.png') }}" alt="Semicirculo">
         <div class="absolute inset-0">
-            <h1 class="text-white font-paytone text-7xl mt-[10%] ml-[5%] ">BAIXE NOSSO APLICATIVO!</h1>
-            <h1 class="text-white font-paytone text-4xl mt-[6%] ml-[5%]">Clique no botão abaixo para fazer o download! ⬎</h1>
+            <h1 class="text-white font-paytone text-6xl mt-[5%] ml-[2%] ">BAIXE NOSSO APLICATIVO!</h1>
+            <h1 class="text-white font-paytone text-4xl mt-[6%] ml-[2%]">Clique no botão abaixo para fazer o download! ⬎</h1>
             <a href="" download>
-                <img class="button-image mt-[5%] ml-[5%] cursor-pointer" src="{{ asset('imagens/botaomaior.png') }}" alt="Botão de Download">
+                <img class="button-image mt-[5%] ml-[2%] cursor-pointer" src="{{ asset('imagens/botaomaior.png') }}" alt="Botão de Download">
             </a>
-            <h1 class="text-white font-Montserrat text-3xl mt-[6%] ml-[5%]">Acesse também as nossas redes sociais!</h1>
-            <img class="button-image mt-[3%] ml-[5%] cursor-pointer" src="{{ asset('imagens/instagram-logo.png') }}" alt="Botão de Download">
+            <h1 class="text-white font-Montserrat text-3xl mt-[2%] ml-[2%]">Acesse também as nossas redes sociais!</h1>
+            <img class="button-image mt-[3%] ml-[2%] cursor-pointer" src="{{ asset('imagens/instagram-logo.png') }}" alt="Botão de Download">
         </div>
+        <img class="object-cover h-[95%]" src="{{ asset('imagens/celular.png') }}" alt="celular">
     </div>
 </x-app-layout>
